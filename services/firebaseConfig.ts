9cd5c3480ae095fcff8c510f24b470064273d62c
Christian <christianvillarragaq@gmail.com>
2026-04-20 15:05:02 -0500

    Arreglo de direccion de bucket Firebase Storage

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC9wfoRApBH-bDBlun_m_L1GBmpFT6KCNQ",
   authDomain: "pilares-inmobiliaria.firebaseapp.com",
   projectId: "pilares-inmobiliaria",
-  storageBucket: "pilares-inmobiliaria.firebasestorage.app",
+  storageBucket: "pilares-inmobiliaria.appspot.com",
   messagingSenderId: "454245759267",
   appId: "1:454245759267:web:acde06a4d136c1ebbb3496",
   measurementId: "G-9ZNBJ394EZ"
