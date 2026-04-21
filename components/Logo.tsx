b50eba7c7d9178519a4189761e84c50ca3d632b1
Christian <christianvillarragaq@gmail.com>
2026-04-21 11:28:16 -0500

    Corregir ruta del logo para GitHub Pages

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className = "h-16", light = false }) => {
   return (
     <div className={`flex items-center ${className}`}>
       <img 
-        src="/logo.jpg" 
+        src="./logo.jpg" 
         alt="Pilares Jurídicos e Inmobiliaria SAS" 
         className="h-full w-auto object-contain"
       />
